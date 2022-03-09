c0224bac20dbebff0d28a925ee77de17837021b9
robmartel <99683835+robmartel@users.noreply.github.com>
2022-03-09 15:33:47 -0500

    added 3 carousel to gallery page but play pause not working properly yet, all the same button

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,34 @@
                 }
             });
         });
+        $(function() {
+            $(".carousel2").carousel( { interval: 5000, pause: "false" } );
+            $("#carouselButtonMenu").click(function(){
+                if ($("#carouselButtonMenu").children("i").hasClass("fa-pause")) {
+                    $(".carousel2").carousel("pause");
+                    $("#carouselButtonMenu").children("i").removeClass("fa-pause");
+                    $("#carouselButtonMenu").children("i").addClass("fa-play");
+                } else {
+                    $(".carousel2").carousel("cycle");
+                    $("#carouselButtonMenu").children("i").removeClass("fa-play");
+                    $("#carouselButtonMenu").children("i").addClass("fa-pause"); 
+                }
+            });
+        });
+        $(function() {
+            $(".carousel3").carousel( { interval: 5000, pause: "false" } );
+            $("#carouselButton3").click(function(){
+                if ($("#carouselButton3").children("i").hasClass("fa-pause")) {
+                    $(".carousel3").carousel("pause");
+                    $("#carouselButton3").children("i").removeClass("fa-pause");
+                    $("#carouselButton3").children("i").addClass("fa-play");
+                } else {
+                    $(".carousel3").carousel("cycle");
+                    $("#carouselButton3").children("i").removeClass("fa-play");
+                    $("#carouselButton3").children("i").addClass("fa-pause"); 
+                }
+            });
+        });
         $(function(){
             $("#reserveButton").click(function(){
                 $("#reserveModal").modal('show')
