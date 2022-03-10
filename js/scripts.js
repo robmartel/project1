c89be9e2429eb619ac77607ef1d3212ccdfaf822
robmartel <99683835+robmartel@users.noreply.github.com>
2022-03-09 19:07:06 -0500

    Added thumbnail photo gallery to gallery page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
      $(function() {
-            $(".carousel").carousel( { interval: 5000, pause: "false" } );
+            $(".carousel").carousel( { interval: 3000, pause: "false" } );
             $("#carouselButton").click(function(){
                 if ($("#carouselButton").children("i").hasClass("fa-pause")) {
                     $(".carousel").carousel("pause");
@@ -13,37 +13,9 @@
                 }
             });
         });
-        $(function() {
-            $(".carousel2").carousel( { interval: 5000, pause: "false" } );
-            $("#carouselButtonMenu").click(function(){
-                if ($("#carouselButtonMenu").children("i").hasClass("fa-pause")) {
-                    $(".carousel2").carousel("pause");
-                    $("#carouselButtonMenu").children("i").removeClass("fa-pause");
-                    $("#carouselButtonMenu").children("i").addClass("fa-play");
-                } else {
-                    $(".carousel2").carousel("cycle");
-                    $("#carouselButtonMenu").children("i").removeClass("fa-play");
-                    $("#carouselButtonMenu").children("i").addClass("fa-pause"); 
-                }
-            });
-        });
-        $(function() {
-            $(".carousel3").carousel( { interval: 5000, pause: "false" } );
-            $("#carouselButton3").click(function(){
-                if ($("#carouselButton3").children("i").hasClass("fa-pause")) {
-                    $(".carousel3").carousel("pause");
-                    $("#carouselButton3").children("i").removeClass("fa-pause");
-                    $("#carouselButton3").children("i").addClass("fa-play");
-                } else {
-                    $(".carousel3").carousel("cycle");
-                    $("#carouselButton3").children("i").removeClass("fa-play");
-                    $("#carouselButton3").children("i").addClass("fa-pause"); 
-                }
-            });
-        });
-        $(function(){
+      $(function(){
             $("#reserveButton").click(function(){
-                $("#reserveModal").modal('show')
+            $("#reserveModal").modal('show')
             });
         });
         $(function() {
